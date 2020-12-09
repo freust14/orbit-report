c19fc34ea89ac36497a9d68584bcdde29734f692
Frank Reust <freust14@gmail.com>
2020-12-08 19:51:32 -0600

    initial view of table

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,6 +11,7 @@ import { Satellite } from './satellite';
 export class AppComponent {
   sourceList: Satellite[];
   title: string = 'orbit-report';
+  
   constructor() {
     this.sourceList =  [
        new Satellite ("SiriusXM", "Communication", "2009-03-21", "LOW", true),
